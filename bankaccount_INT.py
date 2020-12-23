e2ab271b13089e0371b947a51e6c362f58db696f
lzlocki <lzlocki@poczta.fm>
2020-12-23 11:35:25 +0100

    add withdraw method, check methods

--- bankaccount_INT.py
+++ bankaccount_INT.py
@@ -1,8 +1,8 @@
 # Banking task by Lukasz Zlocki / 76103
 # zlocki_76103@cloud.wsb.wroclaw.pl
 
-# ToDo : Do kazdej klasy dodac typ konta(string)(przez override)
-# ToDo : Brak limitu wyplaty - override : withdraw
+# ToDo : Do kazdej klasy dodac typ konta(string)(przez override) / done & checked
+# ToDo : Brak limitu wyplaty - override : withdraw / done & checked
 
 import bankaccount
 
@@ -11,3 +11,26 @@ class BankAccount_INT(bankaccount.BankAccount):
     def __init__(self, bal, na, acctnb, initdate):
         # init superclass
         bankaccount.BankAccount.__init__(self, bal, na, acctnb)
+
+        # init date attribute
+        self.__initdate = initdate
+        self.__acctype = "International bank account"
+
+
+    # -- Overrided methods --
+
+    # Get account type
+    def get_acctype(self):
+        return self.__acctype
+
+    # (Overrided) The withdraw method withdraws an amount
+    # from the account ! without any limits !
+    def withdraw(self, amount):
+        if self.get_balance() >= amount:
+            print("Amount to withdraw : ", amount)
+            _new_balance = self.get_balance()-amount
+            self.set_balance(_new_balance)
+            print("... Amount withdrawn.")
+        else:
+            print('Error: Insufficient funds')
+ 
